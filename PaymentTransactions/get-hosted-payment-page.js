2fd845cf8b5098483b76b56445b4ec1404fa46f5
sunnyrajrathod <sunnyrajrathod@gmail.com>
2016-11-25 15:30:31 +0530

    - Adding sample for get-hosted-payment-page

---
+++ PaymentTransactions/get-hosted-payment-page.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var ApiContracts = require('authorizenet').APIContracts;
+var ApiControllers = require('authorizenet').APIControllers;
+var utils = require('../utils.js');
+var constants = require('../constants.js');
+
+function getHostedPaymentPage(callback) {
+
+	var merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
+	merchantAuthenticationType.setName(constants.apiLoginKey);
+	merchantAuthenticationType.setTransactionKey(constants.transactionKey);
+
+	var transactionRequestType = new ApiContracts.TransactionRequestType();
+	transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION);
+	transactionRequestType.setAmount(utils.getRandomAmount());
+	
+	var setting1 = new ApiContracts.SettingType();
+	setting1.setSettingName('hostedPaymentButtonOptions');
+	setting1.setSettingValue('{\"text\": \"Pay\"}');
+
+	var setting2 = new ApiContracts.SettingType();
+	setting2.setSettingName('hostedPaymentOrderOptions');
+	setting2.setSettingValue('{\"show\": false}');
+
+	var settingList = [];
+	settingList.push(setting1);
+	settingList.push(setting2);
+
+	var alist = new ApiContracts.ArrayOfSetting();
+	alist.setSetting(settingList);
+
+	var getRequest = new ApiContracts.GetHostedPaymentPageRequest();
+	getRequest.setMerchantAuthentication(merchantAuthenticationType);
+	getRequest.setTransactionRequest(transactionRequestType);
+	getRequest.setHostedPaymentSettings(alist);
+
+	//console.log(JSON.stringify(getRequest.getJSON(), null, 2));
+		
+	var ctrl = new ApiControllers.GetHostedPaymentPageController(getRequest.getJSON());
+
+	ctrl.execute(function(){
+
+		var apiResponse = ctrl.getResponse();
+
+		var response = new ApiContracts.GetHostedPaymentPageResponse(apiResponse);
+
+		//pretty print response
+		//console.log(JSON.stringify(response, null, 2));
+
+		if(response != null) 
+		{
+			if(response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK)
+			{
+				console.log('Hosted payment page token :');
+				console.log(response.getToken());
+			}
+			else
+			{
+				//console.log('Result Code: ' + response.getMessages().getResultCode());
+				console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
+				console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+			}
+		}
+		else
+		{
+			console.log('Null response received');
+		}
+
+		callback(response);
+	});
+}
+
+if (require.main === module) {
+	getHostedPaymentPage(function(){
+		console.log('getHostedPaymentPage call complete.');
+	});
+}
+
+module.exports.getHostedPaymentPage = getHostedPaymentPage;
