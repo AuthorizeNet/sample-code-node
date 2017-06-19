ef9e5c2d9e0379b5f47a0ebcb6847e711fe196ef
adavidw <aaron@infocreations.com>
2017-06-19 13:57:28 -0600

    create sample for createAnAcceptPaymentTransaction

---
+++ PaymentTransactions/create-an-accept-payment-transaction.js
@@ -0,0 +1,189 @@
+'use strict';
+
+var ApiContracts = require('authorizenet').APIContracts;
+var ApiControllers = require('authorizenet').APIControllers;
+var SDKConstants = require('authorizenet').Constants;
+var utils = require('../utils.js');
+var constants = require('../constants.js');
+
+function createAnAcceptPaymentTransaction(callback) {
+	var merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
+	merchantAuthenticationType.setName(constants.apiLoginKey);
+	merchantAuthenticationType.setTransactionKey(constants.transactionKey);
+
+	var opaqueData = new ApiContracts.OpaqueDataType();
+	opaqueData.setDataDescriptor('COMMON.ACCEPT.INAPP.PAYMENT');
+	opaqueData.setDataValue('119eyJjb2RlIjoiNTBfMl8wNjAwMDUyN0JEODE4RjQxOUEyRjhGQkIxMkY0MzdGQjAxQUIwRTY2NjhFNEFCN0VENzE4NTUwMjlGRUU0M0JFMENERUIwQzM2M0ExOUEwMDAzNzlGRDNFMjBCODJEMDFCQjkyNEJDIiwidG9rZW4iOiI5NDkwMjMyMTAyOTQwOTk5NDA0NjAzIiwidiI6IjEuMSJ9');
+	
+
+	var paymentType = new ApiContracts.PaymentType();
+	paymentType.setOpaqueData(opaqueData);
+
+	var orderDetails = new ApiContracts.OrderType();
+	orderDetails.setInvoiceNumber('INV-12345');
+	orderDetails.setDescription('Product Description');
+
+	var tax = new ApiContracts.ExtendedAmountType();
+	tax.setAmount('4.26');
+	tax.setName('level2 tax name');
+	tax.setDescription('level2 tax');
+
+	var duty = new ApiContracts.ExtendedAmountType();
+	duty.setAmount('8.55');
+	duty.setName('duty name');
+	duty.setDescription('duty description');
+
+	var shipping = new ApiContracts.ExtendedAmountType();
+	shipping.setAmount('8.55');
+	shipping.setName('shipping name');
+	shipping.setDescription('shipping description');
+
+	var billTo = new ApiContracts.CustomerAddressType();
+	billTo.setFirstName('Ellen');
+	billTo.setLastName('Johnson');
+	billTo.setCompany('Souveniropolis');
+	billTo.setAddress('14 Main Street');
+	billTo.setCity('Pecan Springs');
+	billTo.setState('TX');
+	billTo.setZip('44628');
+	billTo.setCountry('USA');
+
+	var shipTo = new ApiContracts.CustomerAddressType();
+	shipTo.setFirstName('China');
+	shipTo.setLastName('Bayles');
+	shipTo.setCompany('Thyme for Tea');
+	shipTo.setAddress('12 Main Street');
+	shipTo.setCity('Pecan Springs');
+	shipTo.setState('TX');
+	shipTo.setZip('44628');
+	shipTo.setCountry('USA');
+
+	var lineItem_id1 = new ApiContracts.LineItemType();
+	lineItem_id1.setItemId('1');
+	lineItem_id1.setName('vase');
+	lineItem_id1.setDescription('cannes logo');
+	lineItem_id1.setQuantity('18');
+	lineItem_id1.setUnitPrice(45.00);
+
+	var lineItem_id2 = new ApiContracts.LineItemType();
+	lineItem_id2.setItemId('2');
+	lineItem_id2.setName('vase2');
+	lineItem_id2.setDescription('cannes logo2');
+	lineItem_id2.setQuantity('28');
+	lineItem_id2.setUnitPrice('25.00');
+
+	var lineItemList = [];
+	lineItemList.push(lineItem_id1);
+	lineItemList.push(lineItem_id2);
+
+	var lineItems = new ApiContracts.ArrayOfLineItem();
+	lineItems.setLineItem(lineItemList);
+
+	var userField_a = new ApiContracts.UserField();
+	userField_a.setName('A');
+	userField_a.setValue('Aval');
+
+	var userField_b = new ApiContracts.UserField();
+	userField_b.setName('B');
+	userField_b.setValue('Bval');
+
+	var userFieldList = [];
+	userFieldList.push(userField_a);
+	userFieldList.push(userField_b);
+
+	var userFields = new ApiContracts.TransactionRequestType.UserFields();
+	userFields.setUserField(userFieldList);
+
+	var transactionSetting1 = new ApiContracts.SettingType();
+	transactionSetting1.setSettingName('testRequest');
+	transactionSetting1.setSettingValue('s1val');
+
+	var transactionSetting2 = new ApiContracts.SettingType();
+	transactionSetting2.setSettingName('testRequest');
+	transactionSetting2.setSettingValue('s2val');
+
+	var transactionSettingList = [];
+	transactionSettingList.push(transactionSetting1);
+	transactionSettingList.push(transactionSetting2);
+
+	var transactionSettings = new ApiContracts.ArrayOfSetting();
+	transactionSettings.setSetting(transactionSettingList);
+
+	var transactionRequestType = new ApiContracts.TransactionRequestType();
+	transactionRequestType.setTransactionType(ApiContracts.TransactionTypeEnum.AUTHCAPTURETRANSACTION);
+	transactionRequestType.setPayment(paymentType);
+	transactionRequestType.setAmount(utils.getRandomAmount());
+	transactionRequestType.setLineItems(lineItems);
+	transactionRequestType.setUserFields(userFields);
+	transactionRequestType.setOrder(orderDetails);
+	transactionRequestType.setTax(tax);
+	transactionRequestType.setDuty(duty);
+	transactionRequestType.setShipping(shipping);
+	transactionRequestType.setBillTo(billTo);
+	transactionRequestType.setShipTo(shipTo);
+	transactionRequestType.setTransactionSettings(transactionSettings);
+
+	var createRequest = new ApiContracts.CreateTransactionRequest();
+	createRequest.setMerchantAuthentication(merchantAuthenticationType);
+	createRequest.setTransactionRequest(transactionRequestType);
+
+	//pretty print request
+	console.log(JSON.stringify(createRequest.getJSON(), null, 2));
+		
+	var ctrl = new ApiControllers.CreateTransactionController(createRequest.getJSON());
+	//Defaults to sandbox
+	//ctrl.setEnvironment(SDKConstants.endpoint.production);
+
+	ctrl.execute(function(){
+
+		var apiResponse = ctrl.getResponse();
+
+		var response = new ApiContracts.CreateTransactionResponse(apiResponse);
+
+		//pretty print response
+		console.log(JSON.stringify(response, null, 2));
+
+		if(response != null){
+			if(response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK){
+				if(response.getTransactionResponse().getMessages() != null){
+					console.log('Successfully created transaction with Transaction ID: ' + response.getTransactionResponse().getTransId());
+					console.log('Response Code: ' + response.getTransactionResponse().getResponseCode());
+					console.log('Message Code: ' + response.getTransactionResponse().getMessages().getMessage()[0].getCode());
+					console.log('Description: ' + response.getTransactionResponse().getMessages().getMessage()[0].getDescription());
+				}
+				else {
+					console.log('Failed Transaction.');
+					if(response.getTransactionResponse().getErrors() != null){
+						console.log('Error Code: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorCode());
+						console.log('Error message: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorText());
+					}
+				}
+			}
+			else {
+				console.log('Failed Transaction. ');
+				if(response.getTransactionResponse() != null && response.getTransactionResponse().getErrors() != null){
+				
+					console.log('Error Code: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorCode());
+					console.log('Error message: ' + response.getTransactionResponse().getErrors().getError()[0].getErrorText());
+				}
+				else {
+					console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
+					console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+				}
+			}
+		}
+		else {
+			console.log('Null Response.');
+		}
+
+		callback(response);
+	});
+}
+
+if (require.main === module) {
+	createAnAcceptPaymentTransaction(function(){
+		console.log('createAnAcceptPaymentTransaction call complete.');
+	});
+}
+
+module.exports.createAnAcceptPaymentTransaction = createAnAcceptPaymentTransaction;
