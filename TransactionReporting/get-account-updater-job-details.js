9f761a5f8fe8635d4e4ac4d881406070267d89b6
naina <nacharje@visa.com>
2018-11-14 12:36:17 +0530

    PR1992_getaujobdetails-comment

--- TransactionReporting/get-account-updater-job-details.js
+++ TransactionReporting/get-account-updater-job-details.js
@@ -4,21 +4,20 @@ var ApiContracts = require('authorizenet').APIContracts;
 var ApiControllers = require('authorizenet').APIControllers;
 var utils = require('../utils.js');
 var constants = require('../constants.js');
-var Map = require('collections/map');//added to iterate through loop by me
+
 
 function getaccountupdaterJobDetails(callback) {
   var merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
-	merchantAuthenticationType.setName(constants.apiLoginKey);
+    merchantAuthenticationType.setName(constants.apiLoginKey);
 	merchantAuthenticationType.setTransactionKey(constants.transactionKey);
 	var month = '2018-08';
     var modifiedTypeFilter = 'all';
-    var refId='123456'
+    var refId = '123456'
     var paging = new ApiContracts.Paging();
 	paging.setLimit(36);
 	paging.setOffset(1);
 	var subscriptionId = [] ;
-    
-   var getRequest = new ApiContracts.GetAUJobDetailsRequest();
+    var getRequest = new ApiContracts.GetAUJobDetailsRequest();
 	getRequest.setMerchantAuthentication(merchantAuthenticationType);
 	getRequest.setMonth(month);
 	getRequest.setPaging(paging);
@@ -29,21 +28,16 @@ function getaccountupdaterJobDetails(callback) {
       ctrl.execute(function () {
 		var apiResponse = ctrl.getResponse();
 		var response = new ApiContracts.GetAUJobDetailsResponse(apiResponse);
-		//pretty print response
-		//console.log(JSON.stringify(response, null, 2));
+		
 		if (response != null) {
 			if (response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK) {
-				//console.log('Successfully created ' +  response.getAuDetails());
+				
 				if (response.getAuDetails == null)
 					return response;
 				else{
 					console.log('refId :'+response.refId)
 					console.log('Result Code: ' + response.getMessages().getResultCode());
-					console.log('message:')
-				    console.log('Code: ' + response.getMessages().getMessage()[0].getCode());
-				    console.log('text: ' + response.getMessages().getMessage()[0].getText());
-				 //console.log('Successfully created audetails ' + JSON.parse(JSON.stringify(response.getAuDetails().getAuUpdateOrAuDelete())))
-					console.log('total no in result set is :-'+ response.totalNumInResultSet)
+				    console.log('total no in result set is :-'+ response.totalNumInResultSet)
 					console.log('auupdate : ');
 
 				  var audetails = response.getAuDetails()
@@ -52,15 +46,15 @@ function getaccountupdaterJobDetails(callback) {
 				   for (var i=0;i<auupdate.length;i++)
 				    {
 						
-						console.log("**** AU Update Start ****'")
-					 console.log("\t" +'customerProfileID:'+ auupdate[i].getCustomerProfileID());
-					 console.log("\t" +'customerPaymentProfileID:'+ auupdate[i].getCustomerPaymentProfileID());
-					 console.log("\t" +'customerFirstname:'+ auupdate[i].getFirstName());
-					 console.log("\t" +'customerLast name:'+ auupdate[i].getLastName());
-					 console.log("\t" +'UpdateTimeUTC:'+ auupdate[i].getUpdateTimeUTC());
-					 console.log("\t" +'AuReasonCode:'+ auupdate[i].getAuReasonCode());
-					 console.log("\t" +'ReasonDescription:'+ auupdate[i].getReasonDescription());
-                      //if(audetails in ApiContracts.ListOfAUDetailsType.AuUpdateType){
+					 console.log("**** AU Update Start ****'")
+					 console.log("\t" +'Profile ID:'+ auupdate[i].getCustomerProfileID());
+					 console.log("\t" +'Payment Profile ID:'+ auupdate[i].getCustomerPaymentProfileID());
+					 console.log("\t" +'First Name:'+ auupdate[i].getFirstName());
+					 console.log("\t" +'Last Name:'+ auupdate[i].getLastName());
+					 console.log("\t" +'Update Time UTC:'+ auupdate[i].getUpdateTimeUTC());
+					 console.log("\t" +'Reason Code:'+ auupdate[i].getAuReasonCode());
+					 console.log("\t" +'Reason Description:'+ auupdate[i].getReasonDescription());
+                      
 						
 						if ((auupdate[i].getNewCreditCard())!=null){
 							var newcreditcard=auupdate[i].getNewCreditCard()
@@ -84,20 +78,18 @@ function getaccountupdaterJobDetails(callback) {
 							for (var j=0; j<subs.length; j++){
 							console.log("\t" +'SubscriptionId:'+ subs[j])}
 						}  
-						   console.log("**** AU Update END ****'")
+						console.log("**** AU Update END ****'")
 					}
-					
-					  
-					var audelete=audetails.auDelete
+				    var audelete=audetails.auDelete
 					for (var i=0;i< audelete.length;i++){
 							console.log("**** AU Delete Start ****'")
-							console.log("\t" +'customerProfileID:'+  audelete[i].getCustomerProfileID());
-					         console.log("\t" +'customerPaymentProfileID:'+  audelete[i].getCustomerPaymentProfileID());
-					          console.log("\t" +'customerFirstname:'+  audelete[i].getFirstName());
-					          console.log("\t" +'customerLast name:'+  audelete[i].getLastName());
-					          console.log("\t" +'UpdateTimeUTC:'+  audelete[i].getUpdateTimeUTC());
-					         console.log("\t" +'AuReasonCode:'+ audelete[i].getAuReasonCode());
-					          console.log("\t" +'ReasonDescription:'+  audelete[i].getReasonDescription());
+							console.log("\t" +'Profile ID:'+  audelete[i].getCustomerProfileID());
+					         console.log("\t" +'Payment Profile ID:'+  audelete[i].getCustomerPaymentProfileID());
+					          console.log("\t" +'First Name:'+  audelete[i].getFirstName());
+					          console.log("\t" +'Last Name:'+  audelete[i].getLastName());
+					          console.log("\t" +'Update Time UTC:'+  audelete[i].getUpdateTimeUTC());
+					         console.log("\t" +'Reason Code:'+ audelete[i].getAuReasonCode());
+					          console.log("\t" +'Reason Description:'+  audelete[i].getReasonDescription());
 							 
 					        if(( audelete[i].getCreditCard())!=null){
 						         var creditcard= audelete[i].getCreditCard()
@@ -114,10 +106,6 @@ function getaccountupdaterJobDetails(callback) {
 							}  
 							console.log("**** AU Delete END ****'") 
 					}
-					   
-
-						
-					   
 				}
 					
 	        }
@@ -125,18 +113,14 @@ function getaccountupdaterJobDetails(callback) {
 				console.log('Result Code: ' + response.getMessages().getResultCode());
 				console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
 				console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
-				
 			}
 		}
 		else {
 			console.log('Null response received');
 		}
-
-		callback(response);
+       callback(response);
 	});
 }
-
-		
 if (require.main === module) {
 	getaccountupdaterJobDetails(function () {
 		console.log('getAUJobDetails call complete.');
